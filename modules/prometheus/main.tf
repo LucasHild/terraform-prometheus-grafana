2d9a37e21feb2f234e33c2e23f302e103baee660
Lucas Hild <git@lucas-hild.de>
2022-03-29 17:57:58 +0200

    Use templatefile

--- modules/prometheus/main.tf
+++ modules/prometheus/main.tf
@@ -1,8 +1,13 @@
 resource "aws_instance" "instance" {
-  ami                  = var.instance_ami
-  instance_type        = var.instance_type
-  availability_zone    = "eu-central-1c"
-  user_data            = data.template_file.cloud_config_script.rendered
+  ami               = var.instance_ami
+  instance_type     = var.instance_type
+  availability_zone = "eu-central-1c"
+  user_data = templatefile("${path.module}/cloud.conf", {
+    hostname           = var.hostname
+    config_bucket_name = var.config_bucket_name
+    password           = var.password
+    letsencrypt_email  = var.letsencrypt_email
+  })
   security_groups      = [var.security_group_name]
   iam_instance_profile = var.instance_profile
   key_name             = var.key_name
@@ -13,16 +18,5 @@ resource "aws_instance" "instance" {
 
   tags = {
     Name = "monitoring-prometheus"
-  }
-}
-
-data "template_file" "cloud_config_script" {
-  template = file("${path.module}/cloud.conf")
-
-  vars = {
-    hostname           = var.hostname
-    config_bucket_name = var.config_bucket_name
-    password           = var.password
-    letsencrypt_email  = var.letsencrypt_email
   }
 }
